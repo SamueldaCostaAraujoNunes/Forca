9dea87bb0c64b7f99da7bfe0a164d1c0b68afd98
Samuel da Costa Araujo Nunes <samueldacostaaraujonunes@gmail.com>
2020-08-20 18:16:23 -0300

    Leitor de arquivo modifications

--- leitor_de_arquivo.py
+++ leitor_de_arquivo.py
@@ -2,9 +2,9 @@ class Leitor_de_arquivo:
     def __init__(self, path: str):
         self.__path = path
     
-    def ler_arquivo() -> list:
+    def ler_arquivo(self) -> list:
         "Dado o endereço de um arquivo, será retornado uma lista de palavras"
-        with open(nomeArquivo, 'r') as arquivo:
+        with open(self.__path, 'r') as arquivo:
             texto = arquivo.read().strip()
             vetor = texto.split("\n") 
             return vetor
